e335cd74abb4b6ddb7a4f0b019d22b77b75d31fc
Shane <carrollshaned@gmail.com>
2019-01-27 14:29:21 -0500

    export more things at the root level

--- lib/index.ts
+++ lib/index.ts
@@ -1,4 +1,15 @@
 
 export * from '../src/Stage';
-export * from '../src/simulation/Simulation';
+export * from '../src/Color';
 export * from '../src/Mouse';
+export * from '../src/Animation';
+export * from '../src/Timer';
+export * from '../src/shapes/Shape';
+export * from '../src/shapes/Circle';
+export * from '../src/shapes/Rectangle';
+export * from '../src/shapes/Path';
+export * from '../src/shapes/Text';
+export * from '../src/simulation/Simulation';
+export * from '../src/simulation/Environment';
+export * from '../src/simulation/DirectionalMagnitude';
+export * from '../src/simulation/Particle';
